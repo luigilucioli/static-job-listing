771fcf922ce53e6714b355cf1aa3ad47fd60a183
Luigi Lucioli <luigi@studiogusto.com>
2023-03-01 23:02:57 +0100

    Fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bf2f7e3b3eb76ee2"; }
+/******/ 		__webpack_require__.h = function() { return "3c38759758255001"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
